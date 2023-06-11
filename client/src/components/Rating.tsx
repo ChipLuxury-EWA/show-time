de7f320c6bb7a0e97c1c605511b86b3dd1680aee
ChipLuxury-EWA <chip.luxury.ewa@gmail.com>
2023-06-11 22:40:40 +0300

    fix key + removed unused arg

--- client/src/components/Rating.tsx
+++ client/src/components/Rating.tsx
@@ -1,13 +1,13 @@
 import { FaStar, FaStarHalfAlt, FaRegStar } from "react-icons/fa";
 
-const Rating = ({ rate, text }: any) => {
+const Rating = ({ rate }: any) => {
   const dynamicStarsList = [1, 2, 3, 4, 5].map((index) =>
     rate >= index ? (
       <FaStar key={"fullStar" + index} />
     ) : rate >= index - 0.5 ? (
-      <FaStarHalfAlt />
+      <FaStarHalfAlt key={"halfStar" + index} />
     ) : (
-      <FaRegStar key={"halfStar" + index} />
+      <FaRegStar key={"emptyStar" + index} />
     )
   );
 

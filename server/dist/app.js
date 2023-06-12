9ca1a909dd283e91ef65fefaa5a9b3b2c0cc6177
ChipLuxury-EWA <chip.luxury.ewa@gmail.com>
2023-06-12 15:34:24 +0300

    added server

---
+++ server/dist/app.js
@@ -0,0 +1,7 @@
+import express from "express";
+const app = express();
+const port = 3001;
+app.get("/health", (req, res) => {
+    res.send("Api running...");
+});
+app.listen(port, () => console.log(`Server is runnig on port ${port}`));

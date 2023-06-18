80947101140dd14162a74d585e690b5ffaf3fcc0
ChipLuxury-EWA <chip.luxury.ewa@gmail.com>
2023-06-18 10:38:58 +0300

    setup server and added shows routes

--- server/dist/app.js
+++ server/dist/app.js
@@ -1,7 +1,15 @@
 import express from "express";
+import fakeData from "../data/fakeData.js";
 const app = express();
 const port = 3001;
 app.get("/health", (req, res) => {
     res.send("Api running...");
 });
-app.listen(port, () => console.log(`Server is runnig on port ${port}`));
+app.get("/shows", (req, res) => {
+    res.json(fakeData.shows);
+});
+app.get("/shows/:id", (req, res) => {
+    const show = fakeData.shows.find((show) => show._id === req.params.id);
+    res.json(show);
+});
+app.listen(port, () => console.log(`Server is running on port ${port}`));
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,11 @@
 import express from "express";
 
+import fakeData from "../data/fakeData.js";
+
+interface show {
+  _id: string;
+}
+
 const app = express();
 const port = 3001;
 
@@ -7,4 +13,13 @@ app.get("/health", (req, res) => {
   res.send("Api running...");
 });
 
-app.listen(port, () => console.log(`Server is runnig on port ${port}`));
+app.get("/shows", (req, res) => {
+  res.json(fakeData.shows);
+});
+
+app.get("/shows/:id", (req, res) => {
+  const show = fakeData.shows.find((show: show) => show._id === req.params.id);
+  res.json(show);
+});
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));

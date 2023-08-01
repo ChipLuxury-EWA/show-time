3a5a910dfb1c9efad69e5ff6ad4ed634c06e2049
ChipLuxury-EWA <chip.luxury.ewa@gmail.com>
2023-08-01 10:41:40 +0300

    added user not found costume error

--- server/dist/errors/db.errors.js
+++ server/dist/errors/db.errors.js
@@ -11,3 +11,10 @@ export class ShowNotFound extends MongoDBError {
         this.statusCode = 404;
     }
 }
+export class UserNotFound extends MongoDBError {
+    constructor(message = "User not Found") {
+        super(message);
+        this.name = "UserNotFound";
+        this.statusCode = 404;
+    }
+}
--- server/src/errors/db.errors.ts
+++ server/src/errors/db.errors.ts
@@ -12,3 +12,11 @@ export class ShowNotFound extends MongoDBError {
     this.statusCode = 404;
   }
 }
+
+export class UserNotFound extends MongoDBError {
+  constructor(message: string = "User not Found") {
+    super(message);
+    this.name = "UserNotFound";
+    this.statusCode = 404;
+  }
+}

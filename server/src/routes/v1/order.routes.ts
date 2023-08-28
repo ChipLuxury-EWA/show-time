9d7278c546205cb15414ac55a689e944d3abbb4f
ChipLuxury-EWA <chip.luxury.ewa@gmail.com>
2023-08-28 22:49:19 +0300

    added order routes and controller

---
+++ server/src/routes/v1/order.routes.ts
@@ -0,0 +1,49 @@
+import express from "express";
+import { validateAdmin, validateUser } from "../../middleware/auth.middleware.js";
+
+import {
+  getAllOrders,
+  getOrderByID,
+  addNewOrder,
+  updateOrderToPaid,
+  updateOrderToDelivered,
+  getOrdersByUserID,
+} from "../../controllers/order.controller.js";
+const router = express.Router();
+
+router
+    .route("/")
+    .get(validateUser, validateAdmin, getAllOrders)
+    .post(validateUser, addNewOrder)
+    .put()
+    .delete();
+
+router
+    .route("/myorders")
+    .get(validateUser, getOrdersByUserID)
+    .post()
+    .put()
+    .delete();
+
+router
+    .route("/:id")
+    .get(validateUser, validateAdmin, getOrderByID)
+    .post()
+    .put()
+    .delete();
+
+router
+    .route("/:id/paid")
+    .get()
+    .post()
+    .put(validateUser, updateOrderToPaid)
+    .delete();
+
+router
+    .route("/:id/delivered")
+    .get()
+    .post()
+    .put(validateUser, validateAdmin, updateOrderToDelivered)
+    .delete();
+
+export default router;
